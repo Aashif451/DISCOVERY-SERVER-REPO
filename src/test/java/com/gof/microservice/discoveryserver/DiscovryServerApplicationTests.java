510c4cffbb2d95292d3b00ee16039cc8041a0ebb
Aashif <asdf7705889@gmail.com>
2023-02-26 23:03:42 +0530

    project commit

---
+++ src/test/java/com/gof/microservice/discoveryserver/DiscovryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gof.microservice.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscovryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
